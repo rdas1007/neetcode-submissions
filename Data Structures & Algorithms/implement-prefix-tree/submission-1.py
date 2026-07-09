5250c4c8681538d97fd855d311e146ec41985f5e
rdas1007 <rishitdas1007@gmail.com>
2026-07-09 08:42:28 +0100

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,36 @@
+class Node:
+
+    def __init__(self):
+        self.children = {}
+        self.eow = False
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        
+        for w in word:
+            if w not in curr.children:
+                curr.children[w] = Node()
+            curr = curr.children[w]
+        curr.eow = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        
+        for w in word:
+            # print(curr.children)
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        return curr.eow
+
+    def startsWith(self, prefix: str) -> bool:        
+        curr = self.root
+        for w in prefix:
+            if w not in curr.children:
+                return False
+            curr = curr.children[w]
+        return True
